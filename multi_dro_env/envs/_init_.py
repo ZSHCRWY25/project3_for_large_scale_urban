355a4ef687c3c64fcb6e0afc009b860f1af3ec13
ZSHCRWY25 <2388658633@qq.com>
2024-07-02 17:31:14 +0800

    测试注册环境

--- multi_dro_env/envs/_init_.py
+++ multi_dro_env/envs/_init_.py
@@ -2,5 +2,5 @@ from gym.envs.registration import register
 
 register(
     id='mdin-v1',
-    entry_point='gym_env.envs:mdin',
+    entry_point='envs.env:mdin',
 )
--- multi_dro_env/gym_env_test.py
+++ multi_dro_env/gym_env_test.py
@@ -5,18 +5,18 @@ from pathlib import Path
 world_name = 'gym_test_world.yaml'
 # world_name = 'dynamic_obs_test.yaml'
 
-env = gym.make('mrnav-v1', world_name=world_name, robot_number=10, robot_init_mode=3, random_bear=True)
+env = gym.make('mdin-v1', world_name=world_name, drone_number=6)
 env.reset()
 
 for i in range(300):
 
-    vel_list = env.ir_gym.cal_des_omni_list()
+    vel_list = env.ir_gym.cal_des_list()
 
-    obs_list, reward_list, done_list, info_list = env.step_ir(vel_list, vel_type='omni')
+    obs_list, reward_list, done_list, info_list, finish_list = env.step(vel_list)
     id_list=[id for id, done in enumerate(done_list) if done==True]
     
     for id in id_list: 
-        env.reset_one(id) 
+        env.reset_one(id)
 
     env.render()
 
